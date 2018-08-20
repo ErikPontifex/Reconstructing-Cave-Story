a9a1748d210be49205f1a5f3ce9e7ed8114462ab
Erik Pontifex <erikpon@gmail.com>
2018-08-19 18:38:15 -0700

    Initial Commit

---
+++ Cave_Story/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cave_Story
+//
+//  Created by Erik Pontifex on 2018-08-19.
+//  Copyright © 2018 Erik Pontifex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
